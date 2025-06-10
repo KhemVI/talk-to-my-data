681d9030b6b4612e7021870972308696b46360e8
Khem <tempkhem@gmail.com>
2025-06-10 11:20:31 +0700

    feat(*): gitignore & readme.md & stream result

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # talk-to-my-data
+
+Activate virtual environment and install packages on linux
+```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r ./requirements.txt
+```
+
+Run the program
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
-from vector import retriever
+# from vector import retriever
 
-model = OllamaLLM(model="llama3.2")
+model = OllamaLLM(model="deepseek-r1:1.5b")
 
 template = """
 You are an exeprt in answering questions about a pizza restaurant
@@ -21,6 +21,7 @@ while True:
     if question == "q":
         break
     
-    reviews = retriever.invoke(question)
-    result = chain.invoke({"reviews": reviews, "question": question})
-    print(result)
+    # reviews = retriever.invoke(question)
+    for chunk in chain.stream({"reviews": [], "question": question}):
+        print(chunk, end="", flush=True)
+    print()
